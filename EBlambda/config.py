957b870f7ca3d39edaaf7f798da419913b3991a9
akilaprabhavi <akilasiniki@gmail.com>
2025-04-10 15:19:32 +1000

    new configs for API keys

--- EBlambda/config.py
+++ EBlambda/config.py
@@ -12,4 +12,16 @@ CSV_FILE_NAME = "user_prompts"
 IAM_ROLE_NAME = "LambdaS3AccessRole"
 DYNAMO_DB_TABLE="AttackVectors"
 
+AV_URl="https://otx.alienvault.com/api/v1"
+PC_REGION="us-east-1"
+INDEX_N= "alienvault-threats"
+
+openAI_Secret_name="OPENAI_API_KEY_NEW"
+openAI_key_name="OPENAI_API_KEY_NEW"
+
+AV_Secret_name="ALIENVAULT_API_KEY"
+AV_key_name="ALIENVAULT_API_KEY"
+
+PC_Secret_name="PINECONE_API_KEY"
+PC_key_name="PINECONE_API_KEY"
 
